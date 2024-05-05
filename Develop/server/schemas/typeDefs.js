f59a799468f2f45f47f964427a2ce7a9d0f7f2fa
Mark Lewis <mlewis89@gmail.com>
2024-05-06 09:41:57 +1000

    typeDefs

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const typeDefs= `
+    type User {
+        _id: ID!,
+        username: String!,
+        email: String!,
+        bookCount: Int,
+        savedBooks: [Book],
+    }
+
+    type Book {
+        bookId: String!,
+        authors: [String]!,
+        description: String!,
+        title: String!,
+        image: String,
+        link: String,
+    }
+
+    type Auth {
+        token: String!,
+        user: User
+    }
+`;
+
+module.exports= typeDefs;
