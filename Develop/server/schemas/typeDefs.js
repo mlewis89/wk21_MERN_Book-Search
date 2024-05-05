b0108f24ef02b97754f1133532fe19ec00d13e13
Mark Lewis <mlewis89@gmail.com>
2024-05-06 09:56:34 +1000

    typedef q and m

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -16,9 +16,29 @@ const typeDefs= `
         link: String,
     }
 
+    input BookInput{
+        authors: [String]!,
+        description: String!,
+        title: String!,
+        bookId: String!,
+        image: String!, 
+        link: String!,
+    }
+
     type Auth {
-        token: String!,
+        token: ID!
         user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password:String!) : Auth,
+        addUser(username: String!, password: String!) : Auth,
+        saveBook(input: BookInput): User,
+        removeBook(bookId: String!): User,
     }
 `;
 
