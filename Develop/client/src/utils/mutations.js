6c28f052322fb85f3f2bdf3e0e10d31bb750dc06
Mark Lewis <mlewis89@gmail.com>
2024-05-06 16:36:53 +1000

    mutations

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,11 +1,21 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
+mutation Login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
       user {
         _id
+        bookCount
+        email
+        savedBooks {
+          link
+          title
+          image
+          description
+          bookId
+          authors
+        }
         username
       }
     }
@@ -13,29 +23,61 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+mutation AddUser($username: String!, $password: String!) {
+    addUser(username: $username, password: $password) {
       token
       user {
         _id
         username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
       }
     }
   }
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook(
-    $userId: ID!
-    $Book: String!
-    ) 
+mutation Mutation {
+    saveBook {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
   }
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook(
-    $userId: ID!
-    $BookID: String!
-    ) 
+mutation RemoveBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      bookCount
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      username
+    }
   }
 `;
