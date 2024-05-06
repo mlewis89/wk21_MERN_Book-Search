9b1d5eda0597d7190dfae2bc57c2b835824871bf
Mark Lewis <mlewis89@gmail.com>
2024-05-06 11:05:01 +1000

    bug fix in server. js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,7 +1,7 @@
 //import external librairies
 const express = require('express');
-const {ApolloServer} = require('@apolloserver');
-const {expressMIddleware} = require('@apllo/server/express4');
+const {ApolloServer} = require('@apollo/server');
+const {expressMiddleware} = require('@apollo/server/express4');
 const path = require('path');
 
 //import local files
@@ -22,25 +22,27 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/graphql', expressMIddleware(server));
+  app.use('/graphql', expressMiddleware(server));
 
   //if production, server 'dist' folder as static assests
   if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../client/dist')));
   }
 
-  app.get('*'), req, res => {
+  app.get('*'), (req, res) => {
     res.sendFile(path.join(_dirname, '../client/dist/index.html'));
   }
 }
 
 db.once('open', () => {
-  app.listen(PORT, ()=>{
+
     app.listen(PORT, ()=>{
       console.log(`🌍 Now listening on localhost:${PORT}`)
     })
-  })
+
 });
+
+startApolloServer();
 
 /*
 app.use(express.urlencoded({ extended: true }));
