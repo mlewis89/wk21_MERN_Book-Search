34ed98a709555b3b54a4263fd84c892d0949d45f
Mark Lewis <mlewis89@gmail.com>
2024-05-06 10:04:25 +1000

    resolver function place holders

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,1 +1,27 @@
-const {} = require
+const { Query } = require('mongoose');
+const {User, Book} = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async ()=>{
+            return User;
+        }
+    },
+    Mutation:{
+        login: async ()=>{
+            return Auth;
+        },
+        addUser: async ()=>{
+            return Auth;
+        },
+        saveBook: async ()=>{
+            return User;
+        }, 
+        removeBook: async ()=>{
+            return User;
+        },
+    },
+};
+
+module.exports =  resolvers;
