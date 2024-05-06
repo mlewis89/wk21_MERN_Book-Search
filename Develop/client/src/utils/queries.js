46c0d3440af71953087a78cd40a238f639c4d874
Mark Lewis <mlewis89@gmail.com>
2024-05-06 16:31:43 +1000

    basic mutations

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,14 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_GET_ME = gql`
+query user($username: String!) {
+    user(username: $username) {
+        _id
+        username 
+        email
+        bookCount 
+        savedBooks
+      }
+    }
+  }
+`;
