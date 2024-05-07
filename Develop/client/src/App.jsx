de3bea453edca0d1318b5de08d4531f18fe731cc
Mark Lewis <mlewis89@gmail.com>
2024-05-07 17:56:17 +1000

    add jwt to every request

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-const client = new ApolloClient({
+
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const httpLink = createHttpLink({
   uri: '/graphql',
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
